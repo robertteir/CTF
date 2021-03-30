43fdb3c0c477778ff918580c8432d04c11457140
Robert Teir <robert@teir.se>
2021-03-30 23:27:39 +0200

    fix indentation

--- TakeThis.ps1
+++ TakeThis.ps1
@@ -12,84 +12,77 @@ Write-host "   |_|\__,_|_|\_\___|  \__|_| |_|_|___/                             
 Write-host "                                          -PoC 2021-03-30   ""you know a tool is great when it has an ascii-art header""  "
                                                                                                                           
 
-function Get-ExecutedPSScripts
-{
+function Get-ExecutedPSScripts {
     <#
     .SYNOPSIS
     It's dangerous to go alone!
     .DESCRIPTION
     A small tool to find ps1 manipulated.
-    .PARAMETER All
+    .PARAMETER all
     Lists all executions
     .INPUTS
     None.
     .OUTPUTS
-    Might return something useful.
+    Returns a list.
     .EXAMPLE
     PS> Get-ExecutedPSScripts -all
     .LINK
     https://github.com/robertteir
     #>
+
     param(
         [switch] $All
     )
-    
+
     $results = @()
-
     $events= Get-WinEvent -FilterHashTable @{ LogName = "Windows PowerShell"; ID = 400;} | Where-Object {$_.message -like "*.ps1*"}
-
     foreach($event in $events) {
-    if($event.Message.ToString().replace("`t", '').Trim() -match '\b[A-Za-z]:\\.*\.ps1') {
-        $values = [regex]::split($Matches[0], '(-.*[ ])')
-
-        foreach ($value in $values) {
-            if($value -match '[A-Za-z]:\\.*\.ps1') {
-                $file = $Matches[0]
-                $access = $null
-                $can_read = $null
-                $can_write = $null
-
-                if($results.FilePath -notcontains $file) {
-                    $can_read = $true
-                    $can_write = $true
-                    if(Test-Path $file)
-                    {
-                        $access = (Get-Acl $file).AccessToString.Replace("`n",";")
-                        Try {
-                            [io.file]::OpenWrite($file).close() 
-                        } Catch {
-                            $can_write = $false
-                        } 
-                    }
-                    else {
-                        $can_read = $false
-                    }
-                }
-                ElseIf ($all) {
+        if($event.Message.ToString().replace("`t", '').Trim() -match '\b[A-Za-z]:\\.*\.ps1') {
+            $values = [regex]::split($Matches[0], '(-.*[ ])')
+            foreach ($value in $values) {
+                if($value -match '[A-Za-z]:\\.*\.ps1') {
+                    $file = $Matches[0]
+                    $access = $null
                     $can_read = $null
                     $can_write = $null
-                }
-                else {
-                    continue
-                }
-
-                $result = [PSCustomObject]@{
-                    DateTime = $event.TimeCreated
-                    FilePath = $file
-                    CanRead = $can_read
-                    CanWrite = $can_write
-                    Access  = $access
-                }
-                $results += $result
-            }   
+                    if($results.FilePath -notcontains $file) {
+                        $can_read = $true
+                        $can_write = $true
+                        if(Test-Path $file)
+                        {
+                            $access = (Get-Acl $file).AccessToString.Replace("`n",";")
+                            Try {
+                                [io.file]::OpenWrite($file).close() 
+                            } Catch {
+                                $can_write = $false
+                            } 
+                        }
+                        else {
+                            $can_read = $false
+                        }
+                    }
+                    ElseIf ($all) {
+                        $can_read = $null
+                        $can_write = $null
+                    }
+                    else {
+                        continue
+                    }
+                    $result = [PSCustomObject]@{
+                        DateTime = $event.TimeCreated
+                        FilePath = $file
+                        CanRead = $can_read
+                        CanWrite = $can_write
+                        Access  = $access
+                    }
+                    $results += $result
+                }   
+            }
         }
     }
-}
-
     $defaultDisplaySet = 'DateTime','FilePath','CanWrite'
     $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet',[string[]]$defaultDisplaySet)
     $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
     $results | Add-Member MemberSet PSStandardMembers $PSStandardMembers
-
     return $results
 }
